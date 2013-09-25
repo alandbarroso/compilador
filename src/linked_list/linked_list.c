275c2f41df8d2e02fca9c686fe6b1e5d80a0f3a8
alandbarroso <alandbarroso@gmail.com>
2013-09-24 21:05:48 -0300

    Linked list implemented

---
+++ src/linked_list/linked_list.c
@@ -0,0 +1,100 @@
+/*
+ * linked_list.c
+ *
+ *  Created on: Sep 24, 2013
+ *      Author: alan
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked_list.h"
+
+List* create_list(ElementType val, int (*equals_function)(ElementType, ElementType))
+{
+    List* list = (List*) malloc(sizeof(List));
+
+    ListElement* element = (ListElement*) malloc(sizeof(ListElement)); 
+
+    if(list == NULL || element == NULL)
+    {
+        printf("\n List creation failed \n");
+        return NULL;
+    }
+
+    element->val = val;
+    element->next = NULL;
+
+    list->head = element;
+    list->tail = element;
+    list->equals = equals_function;
+
+    return list;
+}
+
+void add_to_list(List* list, ElementType val)
+{
+    if(list == NULL)
+    {
+        printf("\n Node creation failed \n");
+    }
+    else
+    {
+        ListElement *element = (ListElement*) malloc(sizeof(ListElement));
+        
+        if(element == NULL)
+        {
+            printf("\n Node creation failed \n");
+        }
+        else
+        {
+            element->val = val;
+            element->next = NULL;
+        
+            list->tail->next = element;
+            list->tail = element;
+        }
+    }
+}
+
+ListElement* search_in_list(List* list, ElementType val)
+{
+    ListElement* element = list->head;
+
+    while(element != NULL)
+    {
+        if( (*(list->equals))(val, element->val) )
+        {
+            return element;
+        }
+
+        element = element->next;
+    }
+
+    return NULL;
+}
+
+void print_list(List* list)
+{
+    ListElement* element = list->head;
+
+    printf("\n -------Printing list Start------- \n");
+    while(element != NULL)
+    {
+        printf("\n [%d] \n",element->val);
+        element = element->next;
+    }
+    printf("\n -------Printing list End------- \n");
+
+    return;
+}
+
+int eq(ElementType a, ElementType b)
+{
+    if(1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
---
+++ src/linked_list/linked_list.h
@@ -0,0 +1,37 @@
+/*
+ * linked_list.h
+ *
+ *  Created on: Sep 24, 2013
+ *      Author: alan
+ */
+
+#ifndef LINKED_LIST_H_
+#define LINKED_LIST_H_
+
+typedef int ElementType;
+
+typedef struct list_element
+{
+    ElementType val;
+    struct list_element *next;
+} ListElement;
+
+typedef struct
+{
+    ListElement* head;
+    ListElement* tail;
+
+    int (*equals)(ElementType, ElementType);
+} List;
+
+List* create_list(ElementType val, int (*equals_function)(ElementType, ElementType));
+
+void add_to_list(List* list, ElementType val);
+
+ListElement* search_in_list(List* list, ElementType val);
+
+void print_list(List* list);
+
+int eq(ElementType a, ElementType b);
+
+#endif /* LINKED_LIST_H_ */
