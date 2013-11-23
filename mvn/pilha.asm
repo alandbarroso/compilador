e9be596e057c8008a596b7018e07c01089826ebb
alan <alandbarroso@gmail.com>
2013-11-23 15:16:18 -0200

    Stack done

--- mvn/pilha.asm
+++ mvn/pilha.asm
@@ -6,7 +6,7 @@
 
 EMPILHA > ; Funçao de empilhar
 ; PARAMETROS DA FUNCAO EMPILHA
-ENDINICIAL > ; Endereço inicial do bloco a ser empilhado
+END_INICIAL > ; Endereço inicial do bloco a ser empilhado
 TAMANHO > ; Tamanho do bloco a ser copado
 
 
@@ -29,10 +29,12 @@ TAMANHO K /0000
 ; Variaveis auxiliares
 TAMANHO_MVN K /0000
 TOPO_AUX K /0000
+OLD_TOPO K /0000
 
 ; Constantes
 CTE_TAMANHO K =2
-OLD_TOPO K /0000
+END K /F000
+DESLOCA1 K /0010
 
 EMPILHA JP /000
 	; Multiplica o tamanho por 2
@@ -43,13 +45,24 @@ EMPILHA JP /000
 	; Calcula a nova posicao do topo
 	LD TOPO
 	MM OLD_TOPO
-	- TAMANHO_MVN
-	MM TOPO
-	MM TOPO_AUX
+
+	; Verifica se o antigo topo nao é igual a zero
+	JZ LOAD_TOPO 
+		- TAMANHO_MVN
+		MM TOPO
+		MM TOPO_AUX
+
+		JP INIT_COPIA
+	LOAD_TOPO - TAMANHO_MVN 
+		* DESLOCA1 ; move pra esquerda pra tirar um F
+		/ DESLOCA1 ; depois pra direita pra consertar o end
+		
+		MM TOPO
+		MM TOPO_AUX
 
 	; Copiando os arquivos
-	LD TAMANHO_MVN
-	LOOP_ COPIA JZ FIM_COPIA
+	INIT_COPIA LD TAMANHO_MVN
+	LOOP_COPIA JZ FIM_COPIA
 		LD END_INICIAL
 		MM END_ORIGEM
 
@@ -61,7 +74,7 @@ EMPILHA JP /000
 		; Adiciona 2 aos endereços inicial e ao topo aux
 		LD END_INICIAL
 		+ CTE_TAMANHO
-		MM END_INCIAL
+		MM END_INICIAL
 
 		LD TOPO_AUX
 		+ CTE_TAMANHO
@@ -72,15 +85,15 @@ EMPILHA JP /000
 		- CTE_TAMANHO
 		MM TAMANHO_MVN
 
-		JP LOOP_COPIA
+	JP LOOP_COPIA
 	FIM_COPIA LD TOPO
 	+ CTE_TAMANHO
 	MM END_ALVO
 
 	LD OLD_TOPO
-	MM END_ORIGEM
+	MM VALOR
 
-	SC COPIA
+	SC GRAVA
 RS EMPILHA
 
 ;===============================
@@ -140,3 +153,23 @@ ACESSA JP /000
 	; Joga pro acumulador o valor localizado no ponteiro do endereço de origem
 	JOGA_ACM JP /000 ; Don't care
 RS ACESSA
+
+;=======================================================;
+;======================= GRAVA =========================;
+;=======================================================;
+
+VALOR K /0000 ; Valor a ser copiado
+
+GRAVA JP /000
+	; Seta o endereço alvo
+	LD OP_MOVE
+	+ END_ALVO
+	MM COPY_VALOR
+
+	LD VALOR
+
+	; Copia o valor ao endereço alvo
+	COPY_VALOR JP /000 ; Don't care
+RS GRAVA
+
+# PILHA
--- mvn/test.asm
+++ mvn/test.asm
@@ -1,21 +1,85 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;; Teste INPUT/OUTPUT ;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;; Teste ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-READ <
-PRINT <
+; Variaveis vindas da pilha
+EMPILHA <
+END_INICIAL <
+TAMANHO <
+
+DESEMPILHA <
+
+TOPO <
 
 @ /0000
 JP MAIN
 
-AUX K /0000 ; Auxiliar - guarda a leitura da linha de comando
+; bloco 1 a ser empilhado
+TAMANHO1 K =7
+BLOCO1 K /0001
+K /0002
+K /0003
+K /0004
+K /0005
+K /0006
+K /0007
 
-MAIN SC READ
-	MM AUX
-	LD AUX
-	SC PRINT
+; bloco 2 a ser empilhado
+TAMANHO2 K =9
+BLOCO2 K /0008
+K /0009
+K /000A
+K /000B
+K /000C
+K /000D
+$ =2
+K /000E
+
+MAIN LV BLOCO1
+	MM END_INICIAL
+
+	LD TAMANHO1
+	MM TAMANHO
+
+	SC EMPILHA
+
+	LV BLOCO2
+	MM END_INICIAL
+
+	LD TAMANHO2
+	MM TAMANHO
+
+	SC EMPILHA
+
+	SC DESEMPILHA
+
+	LV BLOCO1
+	MM END_INICIAL
+
+	LD TAMANHO1
+	MM TAMANHO
+
+	SC EMPILHA
+
+	LV BLOCO1
+	MM END_INICIAL
+
+	LD TAMANHO1
+	MM TAMANHO
+
+	SC EMPILHA
+
+	SC DESEMPILHA
+
+	LV BLOCO2
+	MM END_INICIAL
+
+	LD TAMANHO2
+	MM TAMANHO
+
+	SC EMPILHA
 ENDMAIN HM MAIN
 
 # MAIN
